5b867824fd0888ade09bdc07a4b0ab6cc33407fc
Connors <brendan.connors@dfo-mpo.gc.ca>
2026-02-17 16:27:01 -0800

    wrangle age comps and write csv for SR modelling

--- R/Nass_age_wrangle.R
+++ R/Nass_age_wrangle.R
@@ -1,11 +1,13 @@
+# script for wrangling raw age data from LGL 
 library(tidyverse)
 library(here)
 library(ggsidekick)
 
+# read in and wrangle data ----
 nass_ages_raw <- read.csv(here("data/Upper lower coastal Nass Chinook ages 1989-2025.csv"))
 
 nass_ages <- nass_ages_raw |>
-  select(Year, CU, Location, MEF, GR_age, Sex) |>
+  select(Year, CU, Location, NFL, MEF, GR_age, Sex) |>
   mutate(salt_age = case_when(GR_age == "21" ~ 1, 
                               GR_age == "31" ~ 2, 
                               GR_age == "41" ~ 3,
@@ -38,7 +40,8 @@ nass_ages <- nass_ages_raw |>
                               Sex == "F" ~ "Female"),
          age = fresh_age + salt_age)
 
-
+# exploratory plots ----
+# age comps by CU
 ggplot(nass_ages, aes(x = Year, fill = as.factor(age))) +
   geom_bar(position = "fill") +
   scale_y_continuous(labels = scales::percent) +
@@ -48,6 +51,7 @@ ggplot(nass_ages, aes(x = Year, fill = as.factor(age))) +
   ylab("") + 
   labs(fill = "Age")
   
+# age comps by sex for upper CU
 ggplot(nass_ages |> filter(CU == "Upper Nass", Sex %in% c("M","F")), aes(x = Year, fill = as.factor(age))) +
   geom_bar(position = "fill") +
   scale_y_continuous(labels = scales::percent) +
@@ -57,35 +61,17 @@ ggplot(nass_ages |> filter(CU == "Upper Nass", Sex %in% c("M","F")), aes(x = Yea
   ylab("") + 
   labs(fill = "Age")
 
+# age comps by location
 ggplot(nass_ages, aes(x = Year, fill = as.factor(age))) +
   geom_bar(position = "fill") +
   scale_y_continuous(labels = scales::percent) +
-  facet_wrap(~Location, nrow  = 2) +
+  facet_wrap(~Location, ncol  = 3) +
   scale_fill_brewer(palette = "Dark2") +
   theme_sleek() +
   ylab("") + 
   labs(fill = "Age")
 
-nass_age_length <- nass_ages |>
-  filter(Sex_full == "Female",
-         age %in% c(4,5,6)) |>
-  group_by(age, Year) |>
-  summarize(length = mean(MEF, na.rm=T))
-
-nass_age_length$age_f <- factor(nass_age_length$age, levels = c(4,5,6))
-
-ggplot(nass_age_length, aes(x = Year, y = length)) +
-  geom_smooth(method="lm", color="grey") +
-  geom_point(size=2, color="dark grey")+
-  xlab("Year") +
-  ylab("Female length \n  (mm; MEFL)") +
-  theme_sleek() +
-  facet_wrap(~age_f, scales = "free_y")
-
-nass_age_length_raw <- nass_ages |>
-  filter(Sex_full == "Female",
-         age %in% c(4,5,6)) 
-
+# female length at age
 ggplot(nass_age_length_raw, aes(x = Year, y = MEF)) +
   stat_summary(fun = mean, geom = "point", size = 3,col="dark grey") + # Add points for the mean
   stat_summary(
@@ -99,3 +85,27 @@ ggplot(nass_age_length_raw, aes(x = Year, y = MEF)) +
   geom_smooth(method = "lm", level = 0.95, color = "dark grey", fill = "lightblue") +
   theme_sleek()
 
+# plot age comps excluding jacks and extremely rare older age classes
+ggplot(nass_ages |> filter(Location == "GW fishwheels", between(age,4,6)), aes(x = Year, fill = as.factor(age))) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  facet_wrap(~CU, nrow  = 2) +
+  scale_fill_brewer(palette = "Dark2") +
+  theme_sleek() +
+  ylab("") + 
+  labs(fill = "Age")
+
+# age comps for spawner recruitment modelling (use GW fishwheels as only source of data across years)
+SR_age_comps <- nass_ages |>
+  filter(Location == "GW fishwheels",
+         between(age,4,6)) |>
+  count(Year, age) |>
+  group_by(Year) |>
+  mutate(year_total = sum(n),
+         prop_count = (n/year_total)*100) |>
+  select(Year, age, prop_count) |>
+  pivot_wider(names_from = age, values_from = prop_count) |>
+  rename(a4 = '4', 
+         a5 = '5', 
+         a6 = '6')
+write.csv(SR_age_comps, here("data/NassAobs.16Feb2026.csv"),row.names = FALSE)
