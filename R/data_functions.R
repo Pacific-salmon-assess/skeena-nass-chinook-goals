06340a14f2afd837cfd4cf9610abb282a9b8a9d8
DylanMG <dylan.glaser1@ucalgary.ca>
2026-03-05 14:39:12 -0800

    get specific ESS for skeena

--- R/data_functions.R
+++ R/data_functions.R
@@ -28,7 +28,7 @@ for(i in 1:dim(skrunchy2025::n_age_observations)[1]){
   a_obs <- mutate(a_obs, year = rownames(a_obs))
   A_obs <- bind_rows(A_obs, a_obs)
 }
-
+ 
 A_obs <- A_obs |>
   mutate(`6` = `6` +`7`) |>
   rename(a4 = `4`, 
@@ -87,6 +87,20 @@ prop_age <- bind_rows(A_obs, nass_A_obs) |> #for plotting later
          a5 = a5/100, 
          a6 = a6/100) |>
   relocate(SMU, .after = 1)
+
+# correct Skeena for uneven ESS - STILL NEED TO DO FOR NASS! 
+ESS_Skeena <- t(skrunchy2025::n_age_observations[,,1] + skrunchy2025::n_age_observations[,,2] + skrunchy2025::n_age_observations[,,2]) |>
+  as.data.frame() |>
+  mutate(year = as.numeric(rownames(t(skrunchy2025::n_age_observations[,,1])))) |>
+  pivot_longer(-year, names_to = "CU") |>
+  rename(ESS = value)
+
+A_obs <- A_obs |>
+left_join(ESS_Skeena, by = c("CU", "year")) |>
+  mutate(a4 = (a4/100)*ESS, #revet fromm scaled to 100 to calculated ESS. 
+         a5 = (a5/100)*ESS, 
+         a6 = (a6/100)*ESS) |>
+  select(-ESS)
 
 A_obs <- bind_rows(A_obs, nass_A_obs) |>
   mutate_if(is.numeric, round, 0)
