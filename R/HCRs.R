a554f21bc413fdfff8c028736feb9bab4ba0375a
Brendan Connors <brendan.connors@dfo-mpo.gc.ca>
2026-09-18 14:21:15 -0700

    Update HCRs.R

--- R/HCRs.R
+++ R/HCRs.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(here)
+library(ggrepel)
 source(here("R/data_functions.R"))
 benchmarks <- read.csv(here("data/generated/bench_par_table_forTWG.4Aug2026.csv"))
 
@@ -13,10 +14,12 @@ agg_RR <- sp_har |>
 
 
 ggplot(data = agg_RR, aes(x = Run, y = ER, col = year)) +
-  geom_point() +
+  geom_point(size=3) +
   labs(x = "Run-size", y = "Exploitation rate") +
   coord_cartesian(ylim=c(0,1), xlim=c(0,250000)) +
-  theme_sleek()
+  theme_sleek() +
+  geom_text_repel(aes(label = year), size=3)
+
 
 ggsave("plots/Skeena/realized-HCR.jpeg", width = 6, height=4,units="in", dpi=600)
 
@@ -34,6 +37,9 @@ ggplot(data = agg_RR, aes(x = spwn, y = ER, col = year)) +
   theme_sleek() +
   geom_hline(yintercept = agg_Umsy$median, lty=2, col = "grey") +
   geom_vline(xintercept = agg_Smsy$median, lty=2, col = "grey") 
+
+ggplot(df, aes(x = x, y = y)) +
+  geom_point(size = 3) +
   
 
 # Nass -----
@@ -44,10 +50,13 @@ agg_RR <- sp_har |>
 
 
 ggplot(data = agg_RR, aes(x = Run, y = ER, col = year)) +
-  geom_point() +
+  geom_point(size=3) +
   labs(x = "Run-size", y = "Exploitation rate") +
-  coord_cartesian(ylim=c(0,1), xlim=c(0,50000)) +
-  theme_sleek()
+  coord_cartesian(ylim=c(0,1), xlim=c(0,60000)) +
+  theme_sleek()+
+  geom_text_repel(aes(label = year), size=3)
+
+ggsave("plots/Nass/realized-HCR.jpeg", width = 6, height=4,units="in", dpi=600)
 
 
 agg_Umsy <- benchmarks |>
